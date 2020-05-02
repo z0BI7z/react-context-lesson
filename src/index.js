36c43940d354e2aff0b2970e07cdef8290013853
kuchi <kuchi@kuchisMBPsmall.fios-router.home>
2020-05-01 21:10:06 -0400

    remove redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import CartProvider from './providers/cart/cart.provider';
 import CollectionsProvider from './providers/collections/collections.provider';
-
-import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -15,13 +11,9 @@ import App from './App';
 ReactDOM.render(
   <CollectionsProvider>
     <CartProvider>
-      <Provider store={store}>
         <BrowserRouter>
-          <PersistGate persistor={persistor}>
             <App />
-          </PersistGate>
         </BrowserRouter>
-      </Provider>
     </CartProvider>
   </CollectionsProvider>,
   document.getElementById('root')
